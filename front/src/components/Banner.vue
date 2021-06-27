af55b24ae1bda0a5e288e467230d379619abdfe8
Tanja Rietveldt <diafox2015@gmail.com>
2021-06-27 22:41:37 +0200

    add front help components: banner,errorMsg,loader

---
+++ front/src/components/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid">
+      <b-jumbotron header="Ideas" lead="Join us" class="text-center jum-tron">
+        <p>World of ideas runs the world</p>
+        
+        <b-btn variant="success" href="#">Ideas</b-btn>
+    </b-jumbotron>
+    
+  </div>
+</template>
+<script>
+export default {
+  name:'AppBanner'
+}
+</script>
+<style scoped>
+.jum-tron{
+    height:300px;
+}
+.jumbotron{
+    padding:1rem;
+}
+
+</style>
